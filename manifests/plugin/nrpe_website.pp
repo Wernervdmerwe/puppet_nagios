9d731587c613664efb695805b3a05bd17a6a77d8
Werner vd Merwe <wernervdmerwe@gmail.com>
2016-09-14 20:07:36 +1200

    Create Nagios Website Puygin

--- manifests/plugin/nrpe_website.pp
+++ manifests/plugin/nrpe_website.pp
@@ -4,15 +4,13 @@
 #To use the --no-check-certificate option configure as follows:
 #command_line $USER1$/check_website_response.sh -u $ARG1$ -w $ARG2$ -c $ARG3$ $ARG4$
 #check_command check_website_response!"http://www.domain.com/index.html"!3000!4000!-nocert
-
+#bash check_website_response.sh -w 2000 -c 5000 -u "https://security.education.govt.nz"
 
 class nagios::plugin::nrpe_website(
   $warn = 2000,
   $crit = 5000,
   $weburl
 ){
-
-  notify {"Setting up for $weburl":}
 
 # NRPE Command
   nrpe::command { 'check_website_response':
