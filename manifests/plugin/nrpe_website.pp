d3474b0f8a6fbb361d019f5714d16a4834609451
Werner vd Merwe <wernervdmerwe@gmail.com>
2016-09-14 19:59:00 +1200

    Create Nagios Website Plugin

--- manifests/plugin/nrpe_website.pp
+++ manifests/plugin/nrpe_website.pp
@@ -12,12 +12,14 @@ class nagios::plugin::nrpe_website(
   $weburl
 ){
 
-## NRPE Command
-#  nrpe::command { 'check_website_response':
-#    ensure  => present,
-#    command => "check_website_response.sh -w ${warn} -c ${crit}";
-#  }
-#
+  notify {"Setting up for $weburl":}
+
+# NRPE Command
+  nrpe::command { 'check_website_response':
+    ensure  => present,
+    command => "check_website_response.sh -w ${warn} -c ${crit}";
+  }
+
 ## Nagios Check
 #  @@nagios_service {"Check Site $::hostname $site":
 #    check_command => "check_nrpe!check_website_response -u $site",
