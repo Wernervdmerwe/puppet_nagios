4e9dff23f7c408d56b0b6fc836dfa3361e68f660
Puppetmaster <dlunixsystemadmins@education.govt.nz>
2017-07-28 14:26:51 +1200

    Exclude pseudo filesystems

--- manifests/plugin/nrpe_disks.pp
+++ manifests/plugin/nrpe_disks.pp
@@ -6,7 +6,7 @@ class nagios::plugin::nrpe_disks (
 # NRPE Command
   nrpe::command { 'check_disks':
     ensure  => present,
-    command => "check_disk -l -L -w $warn -c $crit -e -f -M -A";
+    command => "check_disk -l -L -w $warn -c $crit -e -f -M -A -X configfs -X cgroup -X selinuxfs -X sysfs -X proc -X mqueue -X binfmt_misc -X devtmpfs";
   }
 
 # Nagios Check
