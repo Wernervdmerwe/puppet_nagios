b017f15dd1ec2f6cea025287b782813325e10f02
Werner vd Merwe <werner.vandermerwe@education.govt.nz>
2018-05-07 12:14:39 +1200

    created nagios check for yum patching_state.tmp file

---
+++ files/check_yum_file.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+#
+# Initial author : Diego Martin Gardella [dgardella@gmail.com]
+# Modified by Erling Ouweneel to switch OK and CRITICAL
+# Modified by Jasper Connery to check the file for nagios status and display relevant line, also
+# updated the syntax of a couple of commands
+#
+# Desc : Plugin to verify if a file exists
+#
+# 
+
+PROGNAME=$(basename $0)
+PROGPATH=$(echo $0 | sed -e 's,[\\/][^\\/][^\\/]*$,,')
+TMP_FILE="/tmp/patching_state.tmp"
+
+if [ "$1" = "" ]; then
+  echo -e " Use : $PROGNAME -- Ex : $PROGNAME /etc/hosts \n "
+  exit 3
+fi
+
+if [ -f $1 ]; then
+  if grep -Fq "CRIT:" $TMP_FILE; then
+    echo $(cat $TMP_FILE | grep "CRIT")
+    exit 1
+  elif grep -Fq "WARN:" $TMP_FILE; then
+    echo $(cat $TMP_FILE | grep "WARN")
+    exit 2
+  else
+    echo "UNKNOWN: $TMP_FILE SYNTAX INCORRECT"
+    exit 3
+  fi
+else
+  echo "OK : $1 Does NOT exists "
+  exit 0
+fi
+
---
+++ manifests/plugin/nrpe_yum_filecheck.pp
@@ -0,0 +1,20 @@
+class nagios::plugin::nrpe_yum_filecheck {
+
+# NRPE Command
+  nrpe::plugin { 'check_yum_file.sh':
+    ensure => present,
+    source => 'puppet:///modules/nagios/check_yum_file.sh',
+    notify => Service['nrpe'],
+  }
+
+  nrpe::command { 'check_yum_file':
+    ensure  => present,
+    command => 'check_yum_file.sh /tmp/patching_state.tmp',
+  }
+
+# Nagios Check
+  @@nagios_service { "check-yum_file_${::hostname}":
+    check_command       => 'check_nrpe!check_yum_file',
+    service_description => 'Yum Patching',
+  }
+}
