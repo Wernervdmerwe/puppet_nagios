f1ae9585f3b575a184c1fe829cabaa455c424699
Jasper Connery <ConneryJ@moe.govt.nz>
2018-09-25 12:22:25 +1200

    PPT-150 - allow check_disks command to be provided via hiera

--- manifests/plugin/nrpe_disks.pp
+++ manifests/plugin/nrpe_disks.pp
@@ -1,12 +1,13 @@
 class nagios::plugin::nrpe_disks (
   $warn = '5%',
-  $crit = '2%'
+  $crit = '2%',
+  $check_disks_command = "check_disk -l -L -w ${warn} -c ${crit} -e -f -M -A -X configfs -X cgroup -X selinuxfs -X sysfs -X proc -X mqueue -X binfmt_misc -X devtmpfs",
 ){
 
 # NRPE Command
   nrpe::command { 'check_disks':
     ensure  => present,
-    command => "check_disk -l -L -w $warn -c $crit -e -f -M -A -X configfs -X cgroup -X selinuxfs -X sysfs -X proc -X mqueue -X binfmt_misc -X devtmpfs";
+    command => $check_disks_command;
   }
 
 # Nagios Check
