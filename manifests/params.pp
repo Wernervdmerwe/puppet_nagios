2a1b5484d8939d9f27cd903d27de8faba9209a3a
Enda Fleming <enda.fleming@education.govt.nz>
2019-06-24 14:58:52 +1200

    Moved graphios configuration into nagios.cfg. Added class variables to the template.

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,4 +5,11 @@ class nagios::params{
   $slack_service_channel = '#nagios-alerts'
   $slack_host_channel = '#nagios-alerts'
   $puppetdb_check_enable = false
+  $config_dir = '/etc/nagios/conf.d'
+  $log_dir = '/var/log/nagios/nagios.log'
+  $perfdata_dir = '/var/spool/nagios/graphios'
+  $date_format = 'us'
+  $admin_email = 'DLUnixSystemAdmins@education.govt.nz'
+  $debug_level = 256
+  $debug_verbosity = 2
 }
