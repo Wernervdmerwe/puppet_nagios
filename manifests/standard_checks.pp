ceb335d169b5e7febb582263ed9124cdb6cac258
conneryj <jasper.connery@education.govt.nz>
2019-07-17 12:42:27 +1200

    removed the old nrpe_puppet check

--- manifests/standard_checks.pp
+++ manifests/standard_checks.pp
@@ -9,7 +9,7 @@ class nagios::standard_checks {
     include nagios::plugin::nrpe_zombies
     include nagios::plugin::nrpe_procs
     include nagios::plugin::nrpe_memory
-    include nagios::plugin::nrpe_puppet
+    #include nagios::plugin::nrpe_puppet
     include nagios::plugin::check_ssh
     include nagios::plugin::check_ping
   }
