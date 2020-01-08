00463690f81644da813d3911d42edf3dd23a0ad2
Michal Busek <michal.busek@education.govt.nz>
2020-01-08 15:00:34 +1300

    Add uri and service_description for nrpe_website_string check

--- manifests/plugin/nrpe_website_string.pp
+++ manifests/plugin/nrpe_website_string.pp
@@ -2,15 +2,21 @@
 # # Hiera config example:
 # nagios::plugin::nrpe_website_string::item_list:
 #   - url: 'google.com'
+#     uri: '/'
 #     string: 'google'
 #     warn_limit_s: '2'
 #     crit_limit_s: '5'
 #     timeout: '10'
+#     onredirect: 'follow'
+#     service_description: 'Website_string google on google.com'
 #   - url: 'duckduckgo.com'
+#     uri: '/'
 #     string: 'search'
 #     warn_limit_s: '1'
 #     crit_limit_s: '3'
 #     timeout: '10'
+#     onredirect: 'follow'
+#     service_description: 'Website_string search on duckduckgo.com'
 #
 # By default check connects via SSL and follows redirects.
 #
@@ -21,12 +27,14 @@ class nagios::plugin::nrpe_website_string(
   Integer $notification_interval = lookup('nagios::notification_interval'),
   String $notification_period    = lookup('nagios::notification_period'),
   $item_list = [
-    { url          => 'google.com',
-      string       => 'google',
-      warn_limit_s => '3',
-      crit_limit_s => '5',
-      timeout      => '10',
-      onredirect   => 'follow',
+    { url                 => 'google.com',
+      uri                 => '/',
+      string              => 'google',
+      warn_limit_s        => '3',
+      crit_limit_s        => '5',
+      timeout             => '10',
+      onredirect          => 'follow',
+      service_description => 'Website_string google on google.com',
     },
   ],
 ){
@@ -37,16 +45,16 @@ class nagios::plugin::nrpe_website_string(
 # NRPE Command
   nrpe::command { 'check_http':
     ensure  => present,
-    command => 'check_http -H $ARG1$ -s $ARG2$ -w "$ARG3$" -c "$ARG4$" -t "$ARG5$" -f "$ARG6$" -S';
+    command => 'check_http -H $ARG1$ -u "$ARG2$" -s $ARG3$ -w "$ARG4$" -c "$ARG5$" -t "$ARG6$" -f "$ARG7$" -S';
   }
 
 # Nagios Check
   $item_list.each | $item | {
-    $command = "check_nrpe!check_http -a ${item[url]} ${item[string]} ${item[warn_limit_s]} ${item[crit_limit_s]} ${item[timeout]} ${item[onredirect]}"
+    $command = "check_nrpe!check_http -a ${item[url]} ${item[uri]} ${item[string]} ${item[warn_limit_s]} ${item[crit_limit_s]} ${item[timeout]} ${item[onredirect]}"
 
     @@nagios_service {"Website_string ${item[string]} on ${item[url]} from ${::hostname}":
       check_command         => $command,
-      service_description   => "Website_string ${item[string]} on ${item[url]}",
+      service_description   => $item[service_description],
       host_name             => $::fqdn,
       notify                => Service['nagios'],
       tag                   => $::environment,
