61f3fb19d1f4b14974ab0f5b05b3d5c10c714b10
conneryj <jasper.connery@education.govt.nz>
2020-03-19 09:06:59 +1300

    Created grep_file.sh script and check_gazette_snapshots.pp manifest to manage gazette alerts

---
+++ manifests/plugin/check_gazette_snapshots.pp
@@ -0,0 +1,29 @@
+# Export Nagios service to check yum patching state
+class nagios::plugin::check_gazette_snapshots (
+  Integer $notification_interval = lookup('nagios::notification_interval'),
+  String $notification_period    = lookup('nagios::notification_period'),
+  String $snapshot_errors        = 'Snapshot failed|Crit',
+){
+  # Configure nrpe directories first
+  include nrpe
+
+# NRPE Command
+
+  nrpe::command { 'check_gazette_snapshot':
+    ensure  => present,
+    command => "grep_file.sh '/var/log/gazetteDB.log' '${snapshot_errors}'",
+  }
+
+
+# Nagios Check
+  @@nagios_service { "check yum file ${::hostname}":
+    service_description   => 'EdGazette Snapshot Check',
+    check_command         => 'check_nrpe!check_gazette_snapshot',
+    host_name             => $::fqdn,
+    notify                => Service['nagios'],
+    tag                   => $::environment,
+    notification_interval => $notification_interval,
+    notification_period   => $notification_period,
+    require               => Class['nagios']
+  }
+}
