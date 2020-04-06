8ec232c73dedd9c690121379c474f863aafa87be
Aden Kenny <kennyA@moe.govt.nz>
2020-04-06 14:40:09 +1200

    Add support for excluding mount points

--- manifests/plugin/nrpe_disks.pp
+++ manifests/plugin/nrpe_disks.pp
@@ -3,15 +3,22 @@ class nagios::plugin::nrpe_disks (
   $warn                          = '5%',
   $crit                          = '2%',
   # lint:ignore:140chars
-  $check_disks_command           = "check_disk -l -L -w ${warn} -c ${crit} -e -f -M -A -X configfs -X cgroup -X selinuxfs -X sysfs -X proc -X mqueue -X binfmt_misc -X devtmpfs",
+  $default_check_disks_command = "check_disk -l -L -w ${warn} -c ${crit} -e -f -M -A -X configfs -X cgroup -X selinuxfs -X sysfs -X proc -X mqueue -X binfmt_misc -X devtmpfs",
   # lint:endignore
-  Integer $notification_interval = lookup('nagios::notification_interval'),
-  String $notification_period    = lookup('nagios::notification_period'),
-  String $check_interval         = $nagios::params::check_interval,
-  String $max_check_attempts     = $nagios::params::max_check_attempts,
+  Integer $notification_interval       = lookup('nagios::notification_interval'),
+  String $notification_period          = lookup('nagios::notification_period'),
+  String $check_interval               = $nagios::params::check_interval,
+  String $max_check_attempts           = $nagios::params::max_check_attempts,
+  Array[String] $excluded_mount_points = $nagios::params::excluded_mount_points
 ){
   # Configure nrpe directories first
   include nrpe
+
+  $excluded_mount_point_command = $excluded_mount_points.reduce('') | String $acc, String $mount_point | {
+    "${acc} -i ${mount_point}"
+  }
+
+  $check_disks_command = "${default_check_disks_command}${excluded_mount_point_command}"
 
   # NRPE Command
   nrpe::command { 'check_disks':
