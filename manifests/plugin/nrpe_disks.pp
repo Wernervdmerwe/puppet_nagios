a41adc30dd088527e4401bfb76dd61eb8eb11551
Werner van der Merwe <wernervdmerwe@gmail.com>
2020-07-26 12:09:46 +1200

    Update nrpe_disks.pp
    Remove stat check on remote filesystems (-L)

--- manifests/plugin/nrpe_disks.pp
+++ manifests/plugin/nrpe_disks.pp
@@ -3,7 +3,7 @@ class nagios::plugin::nrpe_disks (
   $warn                          = '5%',
   $crit                          = '2%',
   # lint:ignore:140chars
-  $default_check_disks_command = "check_disk -l -L -w ${warn} -c ${crit} -e -f -M -A -X configfs -X cgroup -X selinuxfs -X sysfs -X proc -X mqueue -X binfmt_misc -X devtmpfs",
+  $default_check_disks_command = "check_disk -l -w ${warn} -c ${crit} -e -f -M -A -X configfs -X cgroup -X selinuxfs -X sysfs -X proc -X mqueue -X binfmt_misc -X devtmpfs",
   # lint:endignore
   Integer $notification_interval       = lookup('nagios::notification_interval'),
   String $notification_period          = lookup('nagios::notification_period'),
